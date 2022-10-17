6b50a8027321166165dd4d3f58a07f295fd443b1
hansheng <hansheng1224@gmail.com>
2022-10-17 14:11:32 +0800

    完成date練習

--- date/index.php
+++ date/index.php
@@ -25,11 +25,23 @@
     echo $now;
     echo "<br>";
     echo date("西元Y年m月d日 H:i:s l", $now);
+    echo "<br>";
+
+    $tomorrow=strtotime("-1 year");
+    echo $tomorrow;
+    echo "<br>";
+    echo date("y-m-d H:i:s");
+    echo "<br>";
+    echo "<br>";
+    echo date("y-m-d H:i:s" , $tomorrow);
+    echo "<br>";
+    $t=date("y-m-d",$tomorrow);
+
     ?>
 
     <h2>練習</h2>
     <ul>
-        <li><a href="">練習一：</a></li>
+        <li><a href="./prac01.php" target: _blank >練習一：</a></li>
         <li><a href="">練習二：</a></li>
         <li><a href="">練習三：</a></li>
         <li><a href="">練習四：</a></li>
